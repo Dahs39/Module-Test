e5e58b44d0f8c9a493b9fb394b04365612bf59de
Dahs39 <homer392001@gmail.com>
2019-11-12 12:00:32 -0800

    Base Working Scripts
    Baseline functional scripts from November 2019

---
+++ moduleTestFunctions.py
@@ -0,0 +1,336 @@
+import pyautogui
+import os
+from time import sleep
+import time
+from pywinauto.findwindows import find_window
+from pywinauto.win32functions import SetForegroundWindow
+from PIL import Image
+from win32api import GetSystemMetrics
+
+# Get resolution of screen the test is running on
+screenWidth = GetSystemMetrics(0)
+screenHeight = GetSystemMetrics(1)
+
+currentDay = time.strftime('%m-%d-%Y')
+
+# Positions of buttons in the Tutor based on screen resolution(david-vbox is at 1920x1157)
+class btnPos:
+	def click_LobbyBtn():
+		#Clicks on Lobby Button
+		lobbyBtnXPos = screenWidth * (25/1920)
+		lobbyBtnYPos = screenHeight * (38/1157)
+		pyautogui.click(x=lobbyBtnXPos, y=lobbyBtnYPos)
+
+	def click_DebugBtn():
+		#Clicks on Debug menu
+		#debugBtnXPos = screenWidth * 0.0505
+		#debugBtnYPos = screenHeight * 0.5056
+		#New Lobby 4/24
+		debugBtnXPos = screenWidth * (84/1920)
+		debugBtnYPos = screenHeight * (609/1157)
+		pyautogui.click(x=debugBtnXPos, y=debugBtnYPos)
+
+	def click_All_ModulesBtn():
+		#Clicks on All Modules
+		#allModulesBtnXPos = screenWidth * 0.0500
+		#allModulesBtnYPos = screenHeight * 0.5523
+		#New Lobby 4/24
+		allModulesBtnXPos = screenWidth * (92/1920)
+		allModulesBtnYPos = screenHeight * (662/1157)
+		pyautogui.click(x=allModulesBtnXPos, y=allModulesBtnYPos)
+
+	def click_Find_Bar():
+		#Clicks on Find bar
+		findBtnXPos = screenWidth * (363/1920)
+		findBtnYPos = screenHeight * (357/1157)
+		pyautogui.click(x=findBtnXPos, y=findBtnYPos)
+
+	def click_Find_Search_Bar():
+		#Clicks on Find Search bar
+		findSearchBtnXPos = screenWidth * (303/1920)
+		findSearchBtnYPos = screenHeight * (393/1157)
+		pyautogui.doubleClick(x=findSearchBtnXPos, y=findSearchBtnYPos)
+
+	def click_Debug_Start_ModuleBtn():
+		#Clicks on Debug Start Module
+		debugStartModuleBtnXPos = screenWidth * (430/1920)
+		debugStartModuleBtnYPos = screenHeight * (943/1157)
+		pyautogui.click(x=debugStartModuleBtnXPos, y=debugStartModuleBtnYPos)
+
+	def click_All_Modules_Start_ModuleBtn():
+		#Clicks on All Modules Start Module
+		allModstartModuleBtnXPos = screenWidth * (488/1920)
+		allModstartModuleBtnYPos = screenHeight * (948/1157)
+		pyautogui.click(x=allModstartModuleBtnXPos, y=allModstartModuleBtnYPos)
+
+	def click_LogOutBtn():
+		#Clicks on Log Out button
+		logOutBtnXPos = screenWidth * 0.9870
+		logOutBtnYPos = screenHeight * 0.0337
+		pyautogui.click(x=logOutBtnXPos, y=logOutBtnYPos)
+		sleep(2)
+
+	def click_LogOutConfirmBtn():
+		#Clicks on Log Out Confirmation button
+		logOutConfirmBtnXPos = screenWidth * 0.9333
+		logOutConfirmBtnYPos = screenHeight * 0.2446
+		pyautogui.click(x=logOutConfirmBtnXPos, y=logOutConfirmBtnYPos)
+		sleep(20)
+
+	def click_ContinueBtn(num_of_Continues):
+		#Clicks on Continue button
+		continueBtnXPos = screenWidth * 0.2318
+		continueBtnYPos = screenHeight * 0.9430
+		count = 0
+		while count < num_of_Continues:
+			sleep(1)
+			pyautogui.click(x=continueBtnXPos, y=continueBtnYPos)
+			sleep(2)
+			count +=1
+
+	def click_CallTutorBtn():
+		#Clicks on Call Tutor button
+		callTutorBtnXPos = screenWidth * 0.1693
+		callTutorBtnYPos = screenHeight * 0.9378
+		sleep(1)
+		pyautogui.click(x=callTutorBtnXPos, y=callTutorBtnYPos)
+		sleep(2)
+
+	def click_AllModulesSearchBar():
+		#Clicks on search bar in All Modules
+		allModsSearchBarBtnXPos = screenWidth * (387/1920)
+		allModsSearchBarYPos = screenHeight * (903/1157)
+		pyautogui.click(x=allModsSearchBarBtnXPos, y=allModsSearchBarYPos)
+		sleep(1)
+
+	def click_MaximizeFrontEndBtn():
+		# maximizeFrontEndBtnXPos = screenWidth * 0.9688
+		# maximizeFrontEndBtnYPos = screenHeight * 0.0147
+		maximizeFrontEndBtnXPos = screenWidth * 0.9568
+		maximizeFrontEndBtnYPos = screenHeight * 0.0190
+		pyautogui.click(x=maximizeFrontEndBtnXPos, y=maximizeFrontEndBtnYPos)
+		sleep(1)
+
+	def click_ReportProblemFixed():
+		reportProblemFixedBtnXPos = screenWidth * 0.2260
+		reportProblemFixedBtnYPos = screenHeight * 0.9408
+		pyautogui.click(x=reportProblemFixedBtnXPos, y=reportProblemFixedBtnYPos)
+		sleep(3)
+
+	def click_ReportProblemFixedConfirm():
+		reportProblemFixedConfirmBtnXPos = screenWidth * 0.2094
+		reportProblemFixedConfirmBtnYPos = screenHeight * 0.8408
+		pyautogui.click(x=reportProblemFixedConfirmBtnXPos, y=reportProblemFixedConfirmBtnYPos)
+		sleep(30)
+
+	def EXTDC_Pos():
+		EXTDC_XPos = screenWidth * 0.8906
+		EXTDC_YPos = screenHeight * 0.5843
+		pyautogui.click(x=EXTDC_XPos, y=EXTDC_YPos)
+		sleep(3)
+
+	def click_StartMenu():
+		Start_XPos = screenWidth * 0.4760
+		Start_YPos = screenHeight * 0.9412
+		pyautogui.click(x=Start_XPos, y=Start_YPos)
+		sleep(1)
+
+	def click_WinAdminTools():
+		AdminTools_XPos = screenWidth * 0.5333
+		AdminTools_YPos	= screenHeight * 0.7087
+		pyautogui.click(x=AdminTools_XPos, y= AdminTools_YPos)
+		sleep(1)
+
+	def DC01_Pos():
+		pyautogui.click(1227, 428)
+		sleep(3)
+
+	def WKS01_Pos():
+		pyautogui.click(1086, 1027)
+		sleep(3)
+
+	def Topology_Pos():
+		pyautogui.click(1342, 1132)
+		sleep(3)
+
+# def Screenshot_with_Paint(name):
+# 	sleep(5)
+# 	pyautogui.hotkey('printscreen')
+# 	sleep(2)
+# 	os.startfile('c:\\Windows\\system32\\mspaint.exe')
+# 	sleep(20)
+# 	pyautogui.hotkey('ctrl', 'v')
+# 	sleep(3)
+# 	pyautogui.hotkey('ctrl', 's')
+# 	sleep(15)
+# 	pyautogui.typewrite('c:\\Python\\Script_Pictures\\moduleTest\\Screenshots\\Unit5\\')
+# 	sleep(1)
+# 	pyautogui.typewrite(currentDay)
+# 	sleep(1)
+# 	pyautogui.typewrite('\\')
+# 	sleep(1)
+# 	pyautogui.typewrite(name)
+# 	sleep(1)
+# 	pyautogui.hotkey('enter')
+# 	sleep(10)
+# 	os.system("taskkill /f /im mspaint.exe")
+# 	sleep(5)
+
+# Logs in to a local frontend with username and password passed through, default is dtl registrar.
+def Local_Frontend_Login(user, password):
+	os.startfile('c:\\runtime\\loginfrontend.bat')
+	sleep(25)
+	btnPos.click_MaximizeFrontEndBtn()
+	pyautogui.typewrite(['f1'])
+	pyautogui.typewrite(user)
+	pyautogui.hotkey('tab'); pyautogui.typewrite(password)
+	pyautogui.typewrite(['enter'])
+	sleep(120)
+
+# def Logout_Frontend():
+# 	btnPos.click_LogOutBtn()
+# 	btnPos.click_LogOutConfirmBtn()
+# 	os.system("taskkill /f /im java.exe")
+# 	Test_Completion_Time = (time.strftime('%m-%d-%Y-%H-%M'))
+# 	Screenshot_with_Paint('testEnd')
+
+# Logs into DC01
+def DC01_Login():
+	btnPos.DC01_Pos()
+	pyautogui.hotkey('ctrl', 'alt', 'backspace')
+	sleep(1)
+	pyautogui.typewrite('student')
+	pyautogui.hotkey('tab'); pyautogui.typewrite('Cangetin00')
+	pyautogui.hotkey('enter')
+	sleep(30)
+	pyautogui.hotkey('ctrl', 'alt', 'backspace')
+	sleep(3)
+	print('Clicking on Task Manager')
+	pyautogui.click(1370, 721)
+	sleep(3)
+	print('Clicking on More Details')
+	pyautogui.click(1020, 734)
+	sleep(3)
+	print('Clicking on CPU Usage Column')
+	pyautogui.click(1212, 501)
+	sleep(3)
+
+# Logs into WKS01
+def WKS01_Login():
+	btnPos.WKS01_Pos()
+	pyautogui.hotkey('ctrl', 'alt', 'backspace')
+	sleep(1)
+	pyautogui.typewrite('student')
+	pyautogui.hotkey('tab'); pyautogui.typewrite('Cangetin00')
+	pyautogui.hotkey('enter')
+	sleep(30)
+	pyautogui.hotkey('ctrl', 'alt', 'backspace')
+	sleep(3)
+	print('Clicking on Task Manager')
+	pyautogui.click(1370, 721)
+	sleep(3)
+	print('Clicking on More Details')
+	pyautogui.click(1020, 734)
+	sleep(3)
+	print('Clicking on CPU Usage Column')
+	pyautogui.click(1212, 501)
+	sleep(3)
+
+# Logs into EXTDC
+def EXTDC_Login():
+	btnPos.EXTDC_Pos()
+	pyautogui.hotkey('ctrl', 'alt', 'backspace')
+	sleep(1)
+	pyautogui.typewrite('student')
+	pyautogui.hotkey('tab'); pyautogui.typewrite('Cangetin00')
+	pyautogui.hotkey('enter')
+	sleep(30)
+	pyautogui.hotkey('ctrl', 'alt', 'backspace')
+	sleep(10)
+	print('Clicking on Task Manager')
+	pyautogui.click(1370, 721)
+	sleep(10)
+	print('Clicking on More Details')
+	pyautogui.click(1065, 787)
+	sleep(10)
+	print('Clicking on CPU Usage Column')
+	pyautogui.click(1276, 553)
+	sleep(3)
+
+# Opens lobby and inputs name of the module in the All Modules search bar
+def selectAllModules(moduleName):
+	sleep(3)
+	btnPos.click_LobbyBtn()
+	sleep(7)
+	btnPos.click_All_ModulesBtn()
+	sleep(2)
+	btnPos.click_AllModulesSearchBar()
+	pyautogui.hotkey('ctrl', 'a'); pyautogui.typewrite(moduleName); pyautogui.hotkey('enter')
+
+# Opens lobby and inputs name of the module in the DEBUG find bar(avoids loading D16 content)
+# Only needed for initial DEBUG one due to FIND bar needing to be expanded
+def firstModule(moduleName):
+	sleep(7)
+	btnPos.click_DebugBtn()
+	sleep(2)
+	btnPos.click_Find_Bar()
+	sleep(2)
+	btnPos.click_Find_Search_Bar()
+	pyautogui.hotkey('ctrl', 'a'); pyautogui.typewrite(moduleName); pyautogui.hotkey('enter')
+	btnPos.click_Debug_Start_ModuleBtn()
+
+# Opens lobby and inputs name of the module in the All Modules find bar
+# Workaround due to not wanting to DEBUG for D16 content
+def firstAllModule(moduleName):
+	sleep(7)
+	btnPos.click_All_ModulesBtn()
+	sleep(2)
+	btnPos.click_AllModulesSearchBar()
+	sleep(2)
+	pyautogui.hotkey('ctrl', 'a'); pyautogui.typewrite(moduleName); pyautogui.hotkey('enter')
+	#btnPos.click_All_Modules_Start_ModuleBtn()
+
+# Opens lobby and inputs name of the module in the DEBUG find bar(avoids loading D16 content)
+def selectAllModules(moduleName):
+	sleep(3)
+	btnPos.click_LobbyBtn()
+	sleep(7)
+	btnPos.click_All_ModulesBtn()
+	sleep(1)
+	btnPos.click_AllModulesSearchBar()
+	pyautogui.hotkey('ctrl', 'a'); pyautogui.typewrite(moduleName); pyautogui.hotkey('enter')
+	#btnPos.click_All_Modules_Start_ModuleBtn()
+
+# Prints screen of current window that is focused, opens paint, and saves it in C:\Script_Pictures with the string that was passed.  Kills the MSPaint process afterwards.
+# def Screenshot_with_Paint(name):
+# 	sleep(5)
+# 	pyautogui.hotkey('printscreen')
+# 	sleep(2)
+# 	os.startfile('c:\\Windows\\system32\\mspaint.exe')
+# 	sleep(20)
+# 	pyautogui.hotkey('ctrl', 'v')
+# 	sleep(3)
+# 	pyautogui.hotkey('ctrl', 's')
+# 	sleep(15)
+# 	pyautogui.typewrite('c:\\Python\\Script_Pictures\\moduleTest\\Screenshots\\EXTDC_Black_Screen_Test\\')
+# 	sleep(1)
+# 	pyautogui.typewrite(currentDay)
+# 	sleep(1)
+# 	pyautogui.typewrite('\\')
+# 	sleep(1)
+# 	pyautogui.typewrite(name)
+# 	sleep(1)
+# 	pyautogui.hotkey('enter')
+# 	sleep(10)
+# 	os.system("taskkill /f /im mspaint.exe")
+# 	sleep(5)
+
+
+def selectEXTDC(moduleName):
+	selectAllModules(moduleName)
+	print('Currently loading: {}'.format(moduleName))
+	print(time.strftime('%m-%d-%Y-%H:%M:%S'))
+	sleep(420)
+	print('Done Sleeping')
+	btnPos.click_ContinueBtn(1)
+	Screenshot_with_Paint(moduleName + count)
---
+++ moduleTestUnit5.py
@@ -0,0 +1,131 @@
+import pyautogui
+import os
+from time import sleep
+import time
+from pywinauto.findwindows import find_window
+from pywinauto.win32functions import SetForegroundWindow
+from PIL import Image
+from moduleTestFunctions import *
+from moduleTestUnitList import *
+
+pyautogui.FAILSAFE = False
+
+# Gets current date where screenshots will be saved in C:\Python\Script_Pictures\moduleTest\Screenshots\
+currentDay = time.strftime('%m-%d-%Y')
+
+# Checks if folder already exists for screenshots, if not, creates it
+filepath = 'c:\\Python\\Script_Pictures\\moduleTest\\Screenshots\\Unit5\\%s' %currentDay
+if not os.path.exists(filepath):
+	os.makedirs('c:\\Python\\Script_Pictures\\moduleTest\\Screenshots\\Unit5\\%s' %currentDay)
+
+def Logout_Frontend():
+	btnPos.click_LogOutBtn()
+	btnPos.click_LogOutConfirmBtn()
+	os.system("taskkill /f /im java.exe")
+	Test_Completion_Time = (time.strftime('%m-%d-%Y-%H-%M'))
+	Screenshot_with_Paint('testEnd')
+	
+def Screenshot_with_Paint(name):
+	sleep(5)
+	pyautogui.hotkey('printscreen')
+	sleep(2)
+	os.startfile('c:\\Windows\\system32\\mspaint.exe')
+	sleep(20)
+	pyautogui.hotkey('ctrl', 'v')
+	sleep(3)
+	pyautogui.hotkey('ctrl', 's')
+	sleep(15)
+	pyautogui.typewrite('c:\\Python\\Script_Pictures\\moduleTest\\Screenshots\\Unit5\\')
+	sleep(1)
+	pyautogui.typewrite(currentDay)
+	sleep(1)
+	pyautogui.typewrite('\\')
+	sleep(1)
+	pyautogui.typewrite(name)
+	sleep(1)
+	pyautogui.hotkey('enter')
+	sleep(10)
+	os.system("taskkill /f /im mspaint.exe")
+	sleep(5)
+
+# Selects module from moduleData and loads it, takes a screenshot after preset sleep time
+def injectModule(moduleName):
+	selectAllModules(moduleName)
+	print('Currently loading: {}'.format(moduleName))
+	print(time.strftime('%m-%d-%Y-%H:%M:%S'))
+	sleep(moduleDataUnit5[moduleName][0])
+	btnPos.click_ContinueBtn(moduleDataUnit5[moduleName][1])
+	if moduleDataUnit5[moduleName][2] == True:
+		btnPos.click_CallTutorBtn()
+	Screenshot_with_Paint(moduleName)
+
+Local_Frontend_Login('dchau', 'Cangetin00')
+
+#Iterates through modules in moduleData dictionary
+for module in moduleDataUnit5:
+	injectModule(module)
+
+sleep(300)
+
+Logout_Frontend()
+
+# #Log out of Tutor
+# btnPos.click_LogOutBtn()
+# btnPos.click_LogOutConfirmBtn()
+# os.system("taskkill /f /im java.exe")
+
+# Test_Completion_Time = (time.strftime('%m-%d-%Y-%H-%M'))
+
+# Screenshot_with_Paint('testEnd')
+
+
+
+
+#7/24 Modifying due to preinjection being turned on
+# moduleDataUnit5= {
+# 			'2K16 Web Server - Introduction': (120, 1, False),
+# 			'2K16 Web Server - Managing Websites 1': (630, 1, False),
+# 			'2K16 Storage - Mirrored Volumes': (90, 0, True),
+# 			'2K16 Print Server - Printer Basics 1': (150, 0, False),
+# 			'2K16 Performance Monitor': (120, 0, False),
+# 			'2K16 Group Policy Troubleshooting - RSOP': (120, 1, False),
+# 			'2K16 Group Policy Deployment - Publish Software': (150, 0, False),
+# 			'2K16 Group Policy - Folder Redirection': (120, 1, False),
+# 			'2K16 Global Catalog - Schema': (120, 0, False),
+# 			'2K16 Global Catalog - Database in the Raw': (120, 1, False),
+# 			'2K16 DHCP - MAC Filtering': (120, 0, False),
+# 			'2K16 Basic OS - Event Viewer 2': (120, 0, False),
+# 			'2K16 Auditing 1': (100, 1, False),
+# 			'2K16 Active Directory Restore Basics': (60, 0, False),
+# 			'2K16 AD Maintenance': (120, 1, False),
+# 			'DNS Server as resolver 2016': (150, 1, False),
+# 			'DNS Zones 2016': (45, 1, False),
+# 			'Rise and Shine Exercise 2016': (200, 1, False),
+# 			'DNS Recursion 2016': (30, 1, False),
+# 			'Adding an Alias Record Exercise 2016': (130, 1, False),
+# 			'Who Hijacked the Mail Server Exercise 2016': (500, 1, False),
+# 			'Introduction to the Domain 2016': (350, 1, False),
+# 			'Installing Admin Tools 2016': (390, 0, False),
+# 			'User Profiles - Change User Name 2016': (400, 0, False),
+# 			'User Profiles - Default User 2016': (120, 1, False),
+# 			'User Profiles - Mandatory Profiles 2016': (460, 1, False),
+# 			'2K16 Adding Counters': (120, 0 , False),
+# 			'2K16 Basic OS - Device Manager': (90, 1, False),
+# 			'2K16 DHCP - Scope Options': (600, 0, False),
+# 			'2K16 File Server Access Management - Group Types and Scopes': (600, 1, False),
+# 			'2K16 Introduction to Active Directory': (600, 1 , False),
+# 			'2K16 DSRM - Authoritative Restore': (900, 2, False),
+# 			'2K16 DSRM - Non-Authoritative Restore': (900, 0 , False),
+# 			'2K16 DHCP - Scopes': (130, 0, False),
+# 			'2K16 Auditing 2': (100, 1, False),
+# 			'2K16 Directing Authentication': (120, 0, False),
+# 			'2K16 Disaster Recovery': (100, 1, False),
+# 			'2K16 Group Policy Security - Administrative Templates': (100, 5, False),
+# 			'2K16 Active Directory Structure - OU Delegation': (130, 0, False),
+# 			'2K16 Web Server - Creating Websites': (615, 1, True),
+# 			'2K16 Web Server - Virtual Directories': (715, 0 , False),
+# 			'2K16 Active Directory Recycle Bin': (210, 4, False),
+# 			'2K16 AD Troubleshooting - ADTD': (160, 4, False),
+# 			'2K16 Group Policy Deployment - Assign Software': (160, 2, False),
+# 			'2K16 Functional Levels - Domain': (700, 0, False),
+# 	}
